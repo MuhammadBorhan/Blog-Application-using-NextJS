94b8ae1947b20838c05fff3af63f4a2ab1c6f12a
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-11-12 15:26:17 +0600

    fake data created for api

---
+++ pages/api/data.js
@@ -0,0 +1,233 @@
+export default {
+  Trending: [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 2,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 3,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 4,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 5,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+  ],
+  Posts: [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 2,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 3,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 4,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 5,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+  ],
+  Popular: [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 2,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 3,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 4,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+    {
+      id: 5,
+      name: "Leanne Graham",
+      username: "Bret",
+      category: "Business, Travel",
+      img: "/images/flower2.webp",
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      published: "Nov 12, 2022",
+      author: {
+        name: "Borhan",
+        img: "/images/borhan.png",
+        designation: "Developer",
+      },
+    },
+  ],
+};
